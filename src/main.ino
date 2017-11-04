9b9ff99c3bc17986a9f0fc950be39ac351ae2d30
wurmtal868 <wurmtal.868@gmail.com>
2017-11-04 23:44:41 +0100

    Major change to everything. Added safety swtich and auto cut detection.

--- src/main.ino
+++ src/main.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
 #include <Hash.h>
+#include "helper.h"
 
 // Includes für display
 
@@ -11,7 +12,7 @@
 
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 13 // Define dummy reset for the OLED display, use JigEnable because it will only disable the motor
+#define OLED_RESET 2 // Define dummy reset for the OLED display, use JigEnable because it will only disable the motor
 Adafruit_SSD1306 display(OLED_RESET); // Create an instance of the OLED display
 
 // #define DEBUG
@@ -19,7 +20,7 @@ Adafruit_SSD1306 display(OLED_RESET); // Create an instance of the OLED display
 
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-#include "credentials.h"
+// #include "credentials.h"
 
 // Add lines below into "credentials.h" with your WiFi credentials
 /*
@@ -31,15 +32,15 @@ const char* password = "*****";
 
 // Jig GPIOs on ESP8266
 
-unsigned char OLED_SDA = 14;
-unsigned char OLED_SCL = 12;
-unsigned char BACK_PIN = 0;     // Backward bridge of L298 and END positions switch
-unsigned char FWD_PIN = 2;      // Foward bridge of L298 and HOME positions switch
-unsigned char sensor_clk = 4;   // Feedback sensor_clk
-unsigned char sensor_dir = 5;   // Feedback sensor_dir
-unsigned char blueKey = 16;     // Blue PushButton(active LOW)
-unsigned char redKey = 15;      // Red PushButton (active HIGH)
-unsigned char JigEnable = 13;   // Enable of L298
+#define OLED_SDA 12
+#define OLED_SCL 13
+#define BACK_PIN 2     // Backward bridge of L298 and END positions switch
+#define FWD_PIN 4      // Foward bridge of L298 and HOME positions switch
+#define sensor_clk 14   // Feedback sensor_clk
+#define sensor_dir 16   // Feedback sensor_dir
+#define blueKey 0     // Blue PushButton(active LOW)
+#define redKey 15      // Red PushButton (active HIGH)
+#define JigEnable 5   // Enable of L298
 
 
 String last_counter="";
@@ -49,6 +50,10 @@ String lastLine2="";
 volatile bool HOME = false ;
 volatile int warte=0;
 volatile int counter = 0;
+String task="";
+
+
+FIFO taskList;
 
 /*
 Show a line of text (t_new) on postion t_x,t_y with textsize t_size on the OLED display
@@ -86,8 +91,8 @@ void display_text(String line1,String line2){
 
 void counter_reset()
 {
-  pinMode(FWD_PIN, OUTPUT);
-  digitalWrite(FWD_PIN,LOW);      // set FWD_PIN LOW to debounce the "HOME" microswtich
+  // pinMode(FWD_PIN, OUTPUT);
+  // digitalWrite(FWD_PIN,LOW);      // set FWD_PIN LOW to debounce the "HOME" microswtich
   detachInterrupt(FWD_PIN);
   counter=0;
   attachInterrupt(sensor_clk, falling_edge, FALLING);
@@ -107,8 +112,7 @@ void display_counter(String statusMSG){
 
 void falling_edge()
 {
-  int other = digitalRead(sensor_dir);
-  if (other == LOW) {
+  if (digitalRead(sensor_dir) == LOW) {
     counter --;
   }
   else {
@@ -127,8 +131,7 @@ void falling_edge()
 
 void rising_edge()
 {
-  int other = digitalRead(sensor_dir);
-  if (other == HIGH ) {
+  if (digitalRead(sensor_dir) == HIGH ) {
     counter --;
   }
   else {
@@ -143,12 +146,28 @@ void rising_edge()
   attachInterrupt(sensor_clk, falling_edge, FALLING ) ; // Change Interupt routine to falling edge to avoid false counting
 }
 
+/*
+Reset Cut list when Red Button is pushed for longer then 1 second
+*/
+
+void resetCutList()
+{
+    while (taskList.isNotEmpty())
+    {
+      taskList.getText();
+    }
+    display_text("Reset", "Cut List") ;
+}
+
+
+
 // Websocket Event Handler
 //
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
 
-    switch(type) {
+    switch(type)
+    {
         case WStype_DISCONNECTED:
 
             break;
@@ -163,64 +182,12 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
             }
             break;
         case WStype_TEXT:
-        {
 
-            String text = String((char *) &payload[0]);
-            display_status(text);
-          if(text=="RESET"){
-            resetJig();
-            String actual = "Position:" + String(counter);
-            char C_actual[16];
-            actual.toCharArray(C_actual,16);
-            webSocket.sendTXT(num, C_actual);
-            }
-          if(text.startsWith("go")){
 
-            String goVal=(text.substring(text.indexOf("g")+2,text.length()));
-            int pos = goVal.toInt();
-
-            Serial.print("Go");
-            Serial.println(goVal);
-            // if ( abs(pos-counter)<=10 and !HOME){
-            //   gopos(pos-25);
-            // }
+            task = String((char *) &payload[0]);
+            // taskList.addText(task);
+            // display_status(task);
             webSocket.sendTXT(num, payload, lenght);
-            gopos(pos);
-            //delay(200);
-            String actual = "Position:" + String(counter);
-            char C_actual[16];
-            actual.toCharArray(C_actual,16);
-            webSocket.sendTXT(num, C_actual);
-            display_text("","Cut!");
-            display_status("Push Blue when done!");
-            while(digitalRead(blueKey)==HIGH)
-            {
-              delay(500);
-            }
-            while(digitalRead(blueKey)==LOW)
-            {
-              delay(300);
-            }
-            break;
-           }
-
-           if(text=="Home"){
-            gopos(-100);
-            gopos(0);
-            display_text("Waiting","for Setup!");
-            }
-
-           if(text=="DONE"){
-            display_text("Done","Cutting");
-            }
-
-
-
-                   }
-
-
-           webSocket.sendTXT(num, payload, lenght);
-          //  webSocket.broadcastTXT(payload, lenght);
             break;
 
         case WStype_BIN:
@@ -235,64 +202,131 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 }
 
 
+void actOnTask(String text)
+{
+  display_status(text);
+  if(text=="RESET")
+  {
+    resetJig();
+    String actual = "Position:" + String(counter);
+    }
+
+  if(text.startsWith("go"))
+  {
+    taskList.addText(text);
+  }
+
+  if(text=="Home")
+  {
+    gopos(-100);
+    delay(100);
+    gopos(0);
+    display_status("HOME done!");
+    // display_text("Waiting","for Setup!");
+  }
+
+  if(text=="DONE")
+  {
+    display_status("CutList complete");
+    // attachInterrupt(blueKey,resetCutList,RISING);
+
+    display_text("Push Blue", "To Start!");
+    waitForBlueButton();
+    if (counter !=0)
+      gopos(-100);
+      gopos(0);
+    while (taskList.isNotEmpty())
+      {
+        text = taskList.getText();
+        display_status(text);
+        String goVal=(text.substring(text.indexOf("g")+2,text.length()));
+        int pos = goVal.toInt();
+
+        Serial.print("Go");
+        Serial.println(goVal);
+        gopos(pos);
+        display_text("","Cut!");
+        Serial.println("Cut now!");
+        waitForJigEnable();
+       /* code */
+      }
+    // detachInterrupt(blueKey);
+    display_text("Done","Cutting");
+    display_status("");
+    delay(1000);
+  }
+}
+
 void gopos(int pos)
 {
-  display_text("WAIT!","");
-  if (pos==-200){
-    if (!HOME and (counter!=0)) {
-      resetJig();
-    }
+  pinMode(JigEnable,INPUT);
+  while (digitalRead(JigEnable)==LOW)
+  {
+    display_text("Move Jig","Back!");
+    delay(100);
   }
-  else {
- //   HOME=false;
-    while (abs(pos-counter)>10){
-      while (pos < (counter-150)){
-      pinMode(BACK_PIN, OUTPUT);
-      digitalWrite(BACK_PIN,LOW);
-      analogWrite(JigEnable,1023);
-      delay (100);
-      }
-      /*
-      while (pos < (counter-30)){
-      pinMode(BACK_PIN, OUTPUT);
-      digitalWrite(BACK_PIN,LOW);
-      analogWrite(JigEnable,750);
-      delay (100);
-      } */
-      while (pos < (counter-3)){
-      pinMode(BACK_PIN, OUTPUT);
-      digitalWrite(BACK_PIN,LOW);
-      analogWrite(JigEnable,(420+(counter-pos)*4));
-      //delay (50);
-      }
-      digitalWrite(BACK_PIN,HIGH);
-      pinMode(BACK_PIN, INPUT);
+  pinMode(JigEnable,OUTPUT);
+  analogWrite(JigEnable,1023);
+  display_text("WAIT!","");
+  yield();
+  if ((pos-counter)<-10)
+  {
+    pinMode(BACK_PIN, OUTPUT);
+    digitalWrite(BACK_PIN,LOW);
+    // analogWrite(JigEnable,1023);
 
-      while (pos > (counter+150)){
-      pinMode(FWD_PIN, OUTPUT);
-      digitalWrite(FWD_PIN,LOW);
-      analogWrite(JigEnable,1023);
-      delay (100);
-      }  /*
-      while (pos > (counter+30)){
-      pinMode(FWD_PIN, OUTPUT);
-      digitalWrite(FWD_PIN,LOW);
-      analogWrite(JigEnable,750);
-      delay (100);
-      }  */
-      while (pos > (counter+3)){
-      pinMode(FWD_PIN, OUTPUT);
-      digitalWrite(FWD_PIN,LOW);
-      analogWrite(JigEnable,(420+(pos-counter)*4));
-      //delay (50);
-
-      }
-      digitalWrite(FWD_PIN,HIGH);
-      pinMode(FWD_PIN, INPUT);
+    while (pos < (counter-300))
+    {
+    // delay (1000);
+    yield();
     }
+
+    while (pos < (counter-150)){
+    delay (500);
+    // yield();
+    }
+    yield();
+    while (pos < (counter-3))
+    {
+    analogWrite(JigEnable,(420+(counter-pos)*4));
+    // delay (10);
+    // yield();
+    }
+
+    digitalWrite(BACK_PIN,HIGH);
+    pinMode(BACK_PIN, INPUT);
+  }
+  if ((pos-counter)>10)
+  {
+    pinMode(FWD_PIN, OUTPUT);
+    digitalWrite(FWD_PIN,LOW);
+    // analogWrite(JigEnable,1023);
+
+    while (pos > (counter+300))
+    {
+      // delay (1000);
+      yield();
+    }
+
+    while (pos > (counter+150))
+    {
+      delay (500);
+      // yield();
+    }
+    yield();
+    while (pos > (counter+3))
+    {
+    analogWrite(JigEnable,(420+(pos-counter)*4));
+    // delay (10);
+    // yield();
+    }
+
+    digitalWrite(FWD_PIN,HIGH);
+    pinMode(FWD_PIN, INPUT);
   }
   analogWrite(JigEnable,1023);
-  delay (200);
+  // pinMode(JigEnable,INPUT);
+  delay (300);
   display_counter(String(counter));
 
 }   // end gopos()
@@ -301,31 +335,32 @@ void resetJig()
 {
   HOME=false;
   display_text("WAIT!","");
-  attachInterrupt(sensor_clk, falling_edge, FALLING);
-  gopos(500);
+  // attachInterrupt(sensor_clk, falling_edge, FALLING);
+  // gopos(500);
+  pinMode(FWD_PIN, INPUT);
   attachInterrupt(FWD_PIN,counter_reset, FALLING);
   pinMode(BACK_PIN, OUTPUT);
   digitalWrite(BACK_PIN,LOW);
   while (digitalRead(FWD_PIN) == HIGH) {
-  delay(100);
+  delay(300);
+  // yield();
   }
-  pinMode(FWD_PIN, INPUT);
+  // pinMode(FWD_PIN, INPUT);
   // detachInterrupt(FWD_PIN);
   digitalWrite(BACK_PIN,HIGH);
   pinMode(BACK_PIN, INPUT);
 
   while (digitalRead(FWD_PIN) == LOW)
   {
-    delay (200);  //warte
+    delay (100);  //warte
   }
 
   //counter=0;
-  //attachInterrupt(sensor_clk, falling_edge, FALLING);
-  /*gopos(100);
-  gopos(0);*/
+  attachInterrupt(sensor_clk, falling_edge, FALLING);
   gopos(150);
-  delay(200);
+  delay(100);
   counter=0;
+  // counter_reset(); // Test
   display_counter(String(counter));
   HOME=true;
   display_text("Waiting","for Setup!");
@@ -333,19 +368,103 @@ void resetJig()
 
 //main loop
 
+// void setupWifi()
+// {
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(ssid1, password1);
+//
+//   while(WiFi.status() != WL_CONNECTED)
+//   {
+//       delay(100);
+//       if(counter==80) {break;} ;
+//       counter +=1;
+//   }
+//   if (WiFi.status() != WL_CONNECTED)
+//   {
+//     Serial.println("Versuche alternatives Netzwerk");
+//
+//     WiFi.begin(ssid, password);
+//
+//     while(WiFi.status() != WL_CONNECTED)
+//       {
+//         delay(100);
+//         if(counter==1000) {break;} ;
+//         counter +=1;
+//       }
+//   }
+//   counter = 0;
+//   Serial.println("");
+//   Serial.print("My IP address:");
+//   Serial.println(WiFi.localIP());
+//   String Message= "IP: " + String (WiFi.localIP()[0]) + "." + String (WiFi.localIP()[1])+ "." + String (WiFi.localIP()[2])+ "." + String( WiFi.localIP()[3]);
+//   display_status(Message);
+// }
+//
+void setupAP()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Box-Joints","");
+  Serial.println("");
+  Serial.print("My IP address:");
+  Serial.println(WiFi.softAPIP());
+  String Message= "IP: " + String (WiFi.softAPIP()[0]) + "." + String (WiFi.softAPIP()[1])+ "." + String (WiFi.softAPIP()[2])+ "." + String( WiFi.softAPIP()[3]);
+  display_status(Message);
+}
 
+void waitForJigEnable()
+{
+  if (digitalRead(redKey)==HIGH)
+  {
+    resetCutList();
+    return;
+  }
+  pinMode(JigEnable,INPUT);
+  while (digitalRead(JigEnable)==HIGH)
+  {
+    delay(100);
+  }
+  display_status("Move Jig Back!");
+  while (digitalRead(JigEnable)==LOW)
+  {
+    delay(100);
+  }
+
+
+}
+
+void waitForBlueButton()
+{
+  display_status("Push Blue when done!");
+  while(digitalRead(blueKey)==HIGH)
+  // while(digitalRead(JigEnable)==HIGH)
+  {
+    delay(100);
+    // yield();
+  }
+  display_status("Release!");
+  while(digitalRead(blueKey)==LOW)
+  // while(digitalRead(JigEnable)==LOW)
+  {
+    delay(300);
+  }
+  display_status("");
+}
 
 void setup()
 {
+  WiFi.mode(WIFI_OFF);
     Serial.begin(115200);
-    pinMode(blueKey,INPUT);
+    pinMode(blueKey,INPUT_PULLUP);
     pinMode(redKey,INPUT);
+    // pinMode(JigEnable,INPUT);
     pinMode(JigEnable,OUTPUT);
 // Setup display
 
   Wire.begin(OLED_SDA,OLED_SCL); // Set I2C pins for OLED display
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // display.display();
+  //
+  // init done
 
   Serial.println("Logo shown");
   display.clearDisplay();
@@ -358,59 +477,69 @@ void setup()
     counter=0;
   Serial.println("\nSW ver:\njig_with_socket_0.1");
   Serial.println("Neustart");
-    WiFi.begin(ssid1, password1);
-
-    while(WiFi.status() != WL_CONNECTED) {
-        delay(100);
-        if(counter==50) {break;} ;
-        counter +=1;
-    }
-    if (WiFi.status() != WL_CONNECTED) {
-      Serial.println("Versuche alternatives Netzwerk");
-
-      WiFi.begin(ssid, password);
-
-      while(WiFi.status() != WL_CONNECTED) {
-          delay(100);
-          if(counter==1000) {break;} ;
-          counter +=1;
-    }}
-    Serial.println("");
-    Serial.print("My IP address:");
-    Serial.println(WiFi.localIP());
-    String Message= "IP: " + String (WiFi.localIP()[0]) + "." + String (WiFi.localIP()[1])+ "." + String (WiFi.localIP()[2])+ "." + String( WiFi.localIP()[3]);
-    display_status(Message);
-    webSocket.begin();
-    webSocket.onEvent(webSocketEvent);
 
 // Jjig setup
-  pinMode(sensor_dir, INPUT);
+pinMode(sensor_dir, INPUT);
+pinMode(sensor_clk, INPUT);
   pinMode(BACK_PIN, INPUT);
   pinMode(FWD_PIN, INPUT);
   Serial.println(" ");
   Serial.println("Fahre Schlitten zurück. Bitte Warten!");
-  //counter=0;
+  counter=0;
+  attachInterrupt(sensor_clk, falling_edge, FALLING);
   resetJig();
   //gopos(50);
   Serial.print("c:");
   Serial.println(counter);
-  display_counter(String(counter));
+  // display_counter(String(counter));
+  display_text("Waiting","for Network!");
+
+  setupAP();
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  display_text("Waiting","for Setup!");
+
 
 }
 
 //
 
-void loop() {
+void loop()
+{
     webSocket.loop();
-    if (warte!=1000){
+    if (warte!=5000){
       warte ++;
     }
     else{
       display_counter(String (counter));
+      display_text("Waiting","for Setup!");
       // display_counter(String (counter)+" = "+String(counter/4)+"mm/10");
       // display_counter(String(counter/4)+"mm/10");
      warte=0;
     }
-    if (digitalRead(redKey)==HIGH) display_counter("Red Button pushed");
+    // if (task!="")
+    // {
+    //   actOnTask(task);
+    //   task="";
+    //   // delay(500);
+    //   // display_status(task);
+    // }
+    if (task!="")
+    {
+      actOnTask(task);
+      task="";
+    }
+
+    yield();
+    if (digitalRead(redKey)==HIGH)
+    {
+      display_counter("Red Button pushed");
+    }
+    yield();
+    pinMode(JigEnable,INPUT);
     if (digitalRead(blueKey)==LOW) display_counter("Blue Button pushed");
-}
+    if (digitalRead(FWD_PIN)==LOW) display_counter("HOME Button pushed");
+    if (digitalRead(BACK_PIN)==LOW) display_counter("END Button pushed");
+    if (digitalRead(JigEnable)==LOW) display_text("Move Jig","Back!");
+  }
